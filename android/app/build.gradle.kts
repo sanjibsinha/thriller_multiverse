44d2c8ca5c4435ea3ef51074565062bc83abcbf0
Sanjib Sinha <sanjib12sinha@gmail.com>
2026-06-13 22:07:45 +0530

    Added chapter reading screen, integrated real QR code and UPI ID for support, and prepared local database structure and adding icons  and more and many more and more and

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.thriller_multiverse"
+    namespace = "in.sanjibdebsinha.thriller_multiverse"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.thriller_multiverse"
+        applicationId = "in.sanjibdebsinha.thriller_multiverse"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
